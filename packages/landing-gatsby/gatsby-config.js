113526b17722f765db5ad292466fb848a7d155f6
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2021-03-14 17:32:59 -0400

    updating

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
+    title: `Joe Hernandez`,
     description: `Roofing Installation, Repair, and Proactive Maintenance Services for all Commercial and Residential Properties in Florida`,
+    location: `Miami`,
     author: `@JohnRRohan`,
   },
   plugins: [
